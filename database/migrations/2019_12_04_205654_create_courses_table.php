2186dc38c40d69db51cc81b08c96b9a6fb03b52e
Jamal Ekpenyong <jamaal.ekpenyong22@gmail.com>
2019-12-05 00:27:42 +0100

    (chore): Creates migrations for models

---
+++ database/migrations/2019_12_04_205654_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->unsignedInteger('duration');
+            $table->unsignedInteger('course_category_id');
+            $table->unsignedInteger('facilitator_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
